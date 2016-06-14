faee789424e3d2dd73c9bc49507e78e78ad0d985
delf <delf@delf-PC>
2016-06-14 23:45:40 +0900

    gg

--- src/word/Item.java
+++ src/word/Item.java
@@ -1,5 +1,15 @@
 package word;
 
-public class Item {
+import java.awt.Point;
 
+public class Item extends Word {
+
+	public Item(String word, int speed, double slope, Point start, int endX) {
+		super(word, speed, slope, start, endX);
+		setItem();
+	}
+
+	private void setItem() {
+		
+	}
 }
